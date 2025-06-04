7a79a4e76a97711ac00d8e3d7d58341c78ceaaf3
quanastic <quanastic@gmail.com>
2025-06-04 20:22:33 +0300

    added favicon

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 800,
+    icon: path.join(__dirname, 'favicon.ico'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
