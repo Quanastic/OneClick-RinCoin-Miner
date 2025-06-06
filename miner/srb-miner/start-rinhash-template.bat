88ac06f3ecba940ea92ba5ab6b1235dbfa464da2
quanastic <quanastic@gmail.com>
2025-06-06 08:16:09 +0300

    changed server

--- miner/srb-miner/start-rinhash-template.bat
+++ miner/srb-miner/start-rinhash-template.bat
@@ -1,5 +1,5 @@
 @echo off
-SRBMiner-MULTI.exe --algorithm rinhash --pool stratum+tcp://rinhash.eu.mine.zergpool.com:7148 --wallet YOUR_WALLET.worker --password c=RIN
+SRBMiner-MULTI.exe --algorithm rinhash --pool stratum.gettomine.com:6498 --wallet YOUR_WALLET.worker --password c=RIN
 pause
 
 
